e4e8b737bf9f27d79e920626dad07ffa50413c1b
prietz01 <p.rietz2001@gmail.com>
2025-09-26 08:35:27 +0200

    Update PKW.cs

--- Fuhrparkverwaltung/Fuhrparkverwaltung/PKW.cs
+++ Fuhrparkverwaltung/Fuhrparkverwaltung/PKW.cs
@@ -8,6 +8,26 @@ namespace Fuhrparkverwaltung
 {
     internal class PKW : Fahrzeug
     {
-        private double ladekapazitaet;
+        private int anzahlTueren;
+
+        public PKW() 
+        {
+        
+        }
+
+        public PKW(string kennzeichen, string hersteller, string modell, int baujahr, int anzahlTueren) : base(kennzeichen, hersteller, modell, baujahr)
+        {
+            this.anzahlTueren = anzahlTueren;
+        }
+
+        public int GetAnzahlTueren()
+        {
+            return anzahlTueren;
+        }
+
+        public void SetAnzahlTueren(int anzahlTueren)
+        {
+            this.anzahlTueren = anzahlTueren;
+        }
     }
 }
