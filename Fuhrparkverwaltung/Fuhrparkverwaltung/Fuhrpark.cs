015bae16d320c0d4c8e4b41ce094389694b4dcfb
prietz01 <p.rietz2001@gmail.com>
2025-09-26 08:46:46 +0200

    Update Fuhrpark.cs

--- Fuhrparkverwaltung/Fuhrparkverwaltung/Fuhrpark.cs
+++ Fuhrparkverwaltung/Fuhrparkverwaltung/Fuhrpark.cs
@@ -8,5 +8,26 @@ namespace Fuhrparkverwaltung
 {
     internal class Fuhrpark
     {
+        private List<Fahrzeug> fuhrparkListe;
+
+        public Fuhrpark()
+        {
+            
+        }
+
+        public Fuhrpark(List<Fahrzeug> fuhrparkListe)
+        {
+            this.fuhrparkListe = fuhrparkListe;
+        }
+
+        public List<Fahrzeug> GetFuhrparkListe()
+        {
+            return fuhrparkListe;
+        }
+
+        public void SetFuhrparkListe(List<Fahrzeug> fuhrparkListe)
+        {
+            this.fuhrparkListe = fuhrparkListe;
+        }
     }
 }
